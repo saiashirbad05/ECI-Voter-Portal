e04e3ade8dcefc001149fd4ff90703cdd99f5a22
Sai Ashirbad Behera <saiashirbadbehera2@gmail.com>
2026-04-30 10:54:54 +0530

    Delete frontend directory

--- frontend/check-icons.cjs
+++
@@ -1,3 +0,0 @@
-const l = require('lucide-react');
-const icons = ['Vote','IndianRupee','Trophy','GraduationCap','TrendingUp','MessageCircle','Upload','Shield','FileText','AtSign','Share2','Link2','LayoutGrid'];
-icons.forEach(n => console.log(n + ': ' + (typeof l[n])));
